0450a61ae73d873aec02f61cbc760d415cfb82ce
Antonio.Barros.Alcantara.Neto <antonio.barros.alcantara.neto@ccc.ufcg.edu.br>
2025-01-23 00:26:18 -0300

    Implementação da questão carrega e não carrega do beecrowd

---
+++ carrrega_ou_nao_carrega/carregaOuNaoCarrega.js
@@ -0,0 +1,16 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+function somaSemCarry(a,b){
+    var result = (a^b);
+    var positivo = result >>> 0;
+    return positivo === 0 ? 0: positivo;
+}
+
+lines.forEach(line => {
+    if(line.trim() !== ''){
+        const [a, b] = line.split(' ').map(Number);
+        const result = somaSemCarry(a, b);
+        console.log(result);
+    }    
+})
